a80b3bce751fbb7ccdb272b89982977deff184a4
Vito Chu <vitochu@VitodeMacBook-Air.local>
2022-09-24 14:11:55 +0800

    DB updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-symmetrical-03910",
     user: "vitochu",
     password: "",
     database: "SmartBrainDB",
