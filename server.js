975ba297dd32d91d25f9a9a369d2474f5780657c
Vito Chu <vitochu@VitodeMacBook-Air.local>
2022-09-24 14:20:04 +0800

    DB fixed

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
